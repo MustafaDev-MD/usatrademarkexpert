33a96d2fd395308f039d27a7d5dc65c8e7dc1293
MustafaDev-MD <mustafadeveloper87@gmail.com>
2026-07-20 23:00:25 +0500

    update popup

--- resources/views/frontend/sections/team-members-section.blade.php
+++ resources/views/frontend/sections/team-members-section.blade.php
@@ -77,6 +77,7 @@
         </div>
 
     </article>
+
     <article class="team-member">
 
         <div class="container">
@@ -115,6 +116,46 @@
         </div>
 
     </article>
+
+    <article class="team-member">
+
+        <div class="container">
+
+            <div class="team-member-inner">
+
+                <div class="team-member-image">
+
+                    <img src="{{ asset('assets/images/our-team/member-1.jpg') }}" alt="">
+
+                    <div class="team-member-info">
+                        <h4>Mary J. Watson</h4>
+                        <span>Paralegal</span>
+                    </div>
+
+                </div>
+
+
+                <div class="team-member-content">
+
+                    <h3>Bona Fide Use</h3>
+
+                    <p>
+                        Customer Trademark Classification: Using the USPTO's ID manual,
+                        our attorneys will devise a class and description of goods and
+                        services that best matches what you seek to protect.
+                        Customer Trademark Classification: Using the USPTO's ID manual,
+                        our attorneys will devise a class and description of goods and
+                        services that best matches what you seek to protect.
+                    </p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </article>
+
     <article class="team-member">
 
         <div class="container">
