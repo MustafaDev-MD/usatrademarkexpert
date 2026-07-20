5d22e486b70652fb9f2dd1afc41e66c7100d6631
MustafaDev-MD <mustafadeveloper87@gmail.com>
2026-07-21 01:14:42 +0500

    add mega menu

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -15,53 +15,168 @@
                 <nav class="nav-menu">
 
                     <ul>
-                        <li>
-                            <a href="{{ route('services') }}"
-                                class="{{ request()->routeIs('services') ? 'active' : '' }}"
+
+                        <li class="has-mega-menu">
+
+                            <span
+                                class="mega-trigger {{ request()->is('services*') ? 'active' : '' }}"
                                 wire:navigate>
                                 Services
-                            </a>
+                                <i class="fa-solid fa-angle-down"></i>
+                            </span>
+
+                            <div class="mega-menu">
+
+                                <div class="mega-menu-grid">
+
+                                    <a href="{{ route('trademark-registration') }}"
+                                        class="{{ request()->routeIs('trademark-registration') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Trademark Registration
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('comprehensive-trademark-search') }}"
+                                        class="{{ request()->routeIs('comprehensive-trademark-search') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Comprehensive Trademark Search
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('trademark-monitoring') }}"
+                                        class="{{ request()->routeIs('trademark-monitoring') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Trademark Monitoring
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('trademark-search') }}"
+                                        class="{{ request()->routeIs('trademark-search') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Free Trademark Search
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('trademark-renewal') }}"
+                                        class="{{ request()->routeIs('trademark-renewal') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Trademark Renewal
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('office-action-response') }}"
+                                        class="{{ request()->routeIs('office-action-response') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Office Action Response
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('statement-of-use') }}"
+                                        class="{{ request()->routeIs('statement-of-use') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Statement of Use
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('filing-an-extension') }}"
+                                        class="{{ request()->routeIs('filing-an-extension') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Filing an Extension
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('copyright-registration') }}"
+                                        class="{{ request()->routeIs('copyright-registration') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Copyright Registration
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('dmca-takedown') }}"
+                                        class="{{ request()->routeIs('dmca-takedown') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        DMCA Takedown
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                </div>
+
+                            </div>
+
                         </li>
 
-                        <li>
-                            <a href="{{ route('trademark-search') }}"
-                                class="{{ request()->routeIs('trademark-search*') ? 'active' : '' }}"
-                                wire:navigate>
-                                Trademark Search
-                            </a>
-                        </li>
-
-                        <li>
+                        <!-- <li>
                             <a href="{{ route('our-team') }}"
                                 class="{{ request()->routeIs('our-team*') ? 'active' : '' }}"
                                 wire:navigate>
                                 Our Team
                             </a>
+                        </li> -->
+
+                        <li class="has-mega-menu">
+
+                            <span
+                                class="mega-trigger {{ request()->routeIs('faqs', 'blog') ? 'active' : '' }}">
+                                Resources
+                                <i class="fa-solid fa-angle-down"></i>
+                            </span>
+
+                            <div class="mega-menu mega-menu-small">
+
+                                <div class="mega-menu-grid">
+
+                                    <a href="{{ route('faqs') }}"
+                                        class="{{ request()->routeIs('faqs') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        FAQs
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('blog') }}"
+                                        class="{{ request()->routeIs('blog') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Blog
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                </div>
+
+                            </div>
+
                         </li>
 
-                        <li>
-                            <a href="{{ route('about') }}"
-                                class="{{ request()->routeIs('about') ? 'active' : '' }}"
-                                wire:navigate>
+                        <li class="has-mega-menu">
+
+                            <span
+                                class="mega-trigger {{ request()->routeIs('about', 'our-team*') ? 'active' : '' }}">
                                 About
-                            </a>
+                                <i class="fa-solid fa-angle-down"></i>
+                            </span>
+
+                            <div class="mega-menu mega-menu-small">
+
+                                <div class="mega-menu-grid">
+
+                                    <a href="{{ route('about') }}"
+                                        class="{{ request()->routeIs('about') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        About Us
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                    <a href="{{ route('our-team') }}"
+                                        class="{{ request()->routeIs('our-team*') ? 'active' : '' }}"
+                                        wire:navigate>
+                                        Our Team
+                                        <i class="fa-solid fa-arrow-right"></i>
+                                    </a>
+
+                                </div>
+
+                            </div>
+
                         </li>
 
-                        <li>
-                            <a href="{{ route('faqs') }}"
-                                class="{{ request()->routeIs('faqs') ? 'active' : '' }}"
-                                wire:navigate>
-                                FAQs
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="{{ route('blog') }}"
-                                class="{{ request()->routeIs('blog') ? 'active' : '' }}"
-                                wire:navigate>
-                                Blog
-                            </a>
-                        </li>
                     </ul>
 
                 </nav>
@@ -98,50 +213,63 @@
 
     <ul>
 
-        <li>
-            <a href="{{ route('services') }}"
-                class="{{ request()->routeIs('services') ? 'active' : '' }}"
-                wire:navigate>
-                Services
-            </a>
+        <!-- Services -->
+        <li class="mobile-dropdown">
+
+            <button class="mobile-dropdown-toggle">
+                <span>Services</span>
+                <i class="fa-solid fa-angle-down"></i>
+            </button>
+
+            <ul class="mobile-submenu">
+
+                <li><a href="{{ route('trademark-registration') }}" wire:navigate>Trademark Registration</a></li>
+                <li><a href="{{ route('comprehensive-trademark-search') }}" wire:navigate>Comprehensive Trademark Search</a></li>
+                <li><a href="{{ route('trademark-monitoring') }}" wire:navigate>Trademark Monitoring</a></li>
+                <li><a href="{{ route('trademark-search') }}" wire:navigate>Free Trademark Search</a></li>
+                <li><a href="{{ route('trademark-renewal') }}" wire:navigate>Trademark Renewal</a></li>
+                <li><a href="{{ route('office-action-response') }}" wire:navigate>Office Action Response</a></li>
+                <li><a href="{{ route('statement-of-use') }}" wire:navigate>Statement of Use</a></li>
+                <li><a href="{{ route('filing-an-extension') }}" wire:navigate>Filing an Extension</a></li>
+                <li><a href="{{ route('copyright-registration') }}" wire:navigate>Copyright Registration</a></li>
+                <li><a href="{{ route('dmca-takedown') }}" wire:navigate>DMCA Takedown</a></li>
+
+            </ul>
+
+        </li>
+
+        <!-- Resources -->
+        <li class="mobile-dropdown">
+
+            <button class="mobile-dropdown-toggle">
+                <span>Resources</span>
+                <i class="fa-solid fa-angle-down"></i>
+            </button>
+
+            <ul class="mobile-submenu">
+                <li><a href="{{ route('faqs') }}" wire:navigate>FAQs</a></li>
+                <li><a href="{{ route('blog') }}" wire:navigate>Blog</a></li>
+            </ul>
+
+        </li>
+
+        <!-- About -->
+        <li class="mobile-dropdown">
+
+            <button class="mobile-dropdown-toggle">
+                <span>About</span>
+                <i class="fa-solid fa-angle-down"></i>
+            </button>
+
+            <ul class="mobile-submenu">
+                <li><a href="{{ route('about') }}" wire:navigate>About Us</a></li>
+                <li><a href="{{ route('our-team') }}" wire:navigate>Our Team</a></li>
+            </ul>
+
         </li>
 
         <li>
-            <a href="{{ route('resources') }}"
-                class="{{ request()->routeIs('resources*') ? 'active' : '' }}"
-                wire:navigate>
-                Resources
-            </a>
-        </li>
-
-        <li>
-            <a href="{{ route('about') }}"
-                class="{{ request()->routeIs('about') ? 'active' : '' }}"
-                wire:navigate>
-                About
-            </a>
-        </li>
-
-        <li>
-            <a href="{{ route('faqs') }}"
-                class="{{ request()->routeIs('faqs') ? 'active' : '' }}"
-                wire:navigate>
-                FAQs
-            </a>
-        </li>
-
-        <li>
-            <a href="{{ route('blog') }}"
-                class="{{ request()->routeIs('blog') ? 'active' : '' }}"
-                wire:navigate>
-                Blog
-            </a>
-        </li>
-
-        <li>
-            <a href="{{ route('contact') }}"
-                class="{{ request()->routeIs('contact') ? 'active' : '' }}"
-                wire:navigate>
+            <a href="{{ route('contact') }}" wire:navigate>
                 Free Consultation
             </a>
         </li>
@@ -149,3 +277,22 @@
     </ul>
 
 </nav>
+<script>
+    document.querySelectorAll(".mobile-dropdown-toggle").forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const parent = button.parentElement;
+
+        document.querySelectorAll(".mobile-dropdown").forEach(item => {
+            if(item !== parent){
+                item.classList.remove("active");
+            }
+        });
+
+        parent.classList.toggle("active");
+
+    });
+
+});
+</script>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,26 @@ Route::view('/about', 'frontend.about')->name('about');
 
 Route::view('/services', 'frontend.services')->name('services');
 
+Route::view('/services/trademark-registration', 'frontend.services.trademark-registration')->name('trademark-registration');
+
+Route::view('/services/trademark-monitoring', 'frontend.services.trademark-monitoring')->name('trademark-monitoring');
+
+Route::view('/services/trademark-renewal', 'frontend.services.trademark-renewal')->name('trademark-renewal');
+
+Route::view('/services/office-action-response', 'frontend.services.office-action-response')->name('office-action-response');
+
+Route::view('/services/filing-an-extension', 'frontend.services.filing-an-extension')->name('filing-an-extension');
+
+Route::view('/services/comprehensive-trademark-search', 'frontend.services.comprehensive-trademark-search')->name('comprehensive-trademark-search');
+
+Route::view('/services/copyright-registration', 'frontend.services.copyright-registration')->name('copyright-registration');
+
+Route::view('/services/trademark-search', 'frontend.trademark-search')->name('trademark-search');
+
+Route::view('/services/statement-of-use', 'frontend.services.statement-of-use')->name('statement-of-use');
+
+Route::view('/services/dmca-takedown', 'frontend.services.dmca-takedown')->name('dmca-takedown');
+
 Route::view('/faqs', 'frontend.faqs')->name('faqs');
 
 Route::view('/resources', 'frontend.resources')->name('resources');
@@ -21,8 +41,6 @@ Route::view('/single-blog', 'frontend.single-blog')->name('single-blog');
 Route::view('/contact', 'frontend.contact')->name('contact');
 
 Route::view('/privacy-policy', 'frontend.privacy-policy')->name('privacy-policy');
-
-Route::view('/trademark-search', 'frontend.trademark-search')->name('trademark-search');
 
 Route::view('/our-team', 'frontend.our-team')->name('our-team');
 
